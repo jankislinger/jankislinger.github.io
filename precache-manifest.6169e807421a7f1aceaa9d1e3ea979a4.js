1253e4d1e95ec61fd6d71088a9d56cc73c2e4b5e
Jan Kislinger <jan.kislinger@gmail.com>
2020-09-25 17:36:44 +0200

    Updates

---
+++ precache-manifest.6169e807421a7f1aceaa9d1e3ea979a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ba8cfbef8fba109b075a3709ff502f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8463301c8882945cda38",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97c26c59c31e0ab2af23",
+    "url": "/static/js/2.bcf4e6b3.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.bcf4e6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8463301c8882945cda38",
+    "url": "/static/js/main.5e9bb77f.chunk.js"
+  },
+  {
+    "revision": "8eb2db9fe47a4017feb7",
+    "url": "/static/js/runtime-main.2dda6dbb.js"
+  }
+]);
